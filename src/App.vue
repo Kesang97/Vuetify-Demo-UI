c137f2ed34048da7b8413c950891bcdcd0e55a82
Kesang97 <kesangs99@gmail.com>
2022-11-16 17:58:29 +0530

    nav-toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Navbar/>
+
+    <v-main>
+      <v-container>
+      <home-demo/>
+    </v-container>
+    </v-main>
+    
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeDemo from './components/home-demo.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HomeDemo,
+    Navbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/home-demo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="homepage">
+      <h1>Homepage</h1>
+      
+      <!-- <v-btn class="pink white--text">Click Me</v-btn>
+      <v-btn depressed color="pink">click me</v-btn>
+      <v-btn plain color="pink">click me</v-btn>
+
+      <v-btn depressed class="pink white--text mr-2">
+        <v-icon left>mdi-email</v-icon>
+        <span>email me</span>
+      </v-btn>
+
+      <v-btn depressed small class="pink white--text mr-2">
+        <v-icon left small>mdi-email</v-icon>
+        <span>email me</span>
+      </v-btn>
+      
+      <v-btn depressed large class="pink white--text mr-3">
+        <span>email me</span>
+        <v-icon right large>mdi-email</v-icon>
+      </v-btn>
+
+      <v-btn  fab depressed small dark color="purple">
+        <v-icon>mdi-heart</v-icon>
+      </v-btn> -->
+      <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
+      <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+      <!-- <v-btn class="hidden-sm-only">click me</v-btn> -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data: () => ({
+    
+    }),
+  }
+</script>
